c38532ac58d7c586e19b2ce0d91692ebf7ba1194
TommeyFaherty <G00344243@gmit.ie>
2020-02-26 21:02:17 +0000

    Script for 3rd person camera and rotating foor
    rotate with i,j or k. Also temporary scripts for player movement on ball

---
+++ Assets/Scripts/FloorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.I))
+        {
+            transform.Rotate(0,0.5f,0);
+        }
+        if(Input.GetKey(KeyCode.L))
+        {
+            transform.Rotate(0,0,0.5f);
+        }
+        if(Input.GetKey(KeyCode.J))
+        {
+            transform.Rotate(0.5f,0,0);
+        }
+    }
+}
