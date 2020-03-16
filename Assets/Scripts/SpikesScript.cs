38feef952e06ed6ad9ef82f08e48306ec3606645
TommeyFaherty <tommeyf3@hotmail.com>
2020-03-16 13:52:41 +0000

    Added script for spike collision

---
+++ Assets/Scripts/SpikesScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpikesScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Player")
+        {
+            //restart level
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
