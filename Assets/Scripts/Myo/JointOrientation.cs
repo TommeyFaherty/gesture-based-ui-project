b5cbedf2ee8de5d5e76ef7b7ad61451e70fc38f4
Ronan-H <g00342393@gmit.ie>
2020-03-20 18:33:40 +0000

    No y axis rotation from my movement

--- Assets/Scripts/Myo/JointOrientation.cs
+++ Assets/Scripts/Myo/JointOrientation.cs
@@ -84,6 +84,8 @@ public class JointOrientation : MonoBehaviour
         // Here the anti-roll and yaw rotations are applied to the myo Armband's forward direction to yield
         // the orientation of the joint.
         Quaternion target = _antiYaw * antiRoll * Quaternion.LookRotation (myo.transform.forward);
+        Vector3 targetEuler = target.eulerAngles;
+        target = Quaternion.Euler(targetEuler.x, 0, targetEuler.y);
 
         // The above calculations were done assuming the Myo armbands's +x direction, in its own coordinate system,
         // was facing toward the wearer's elbow. If the Myo armband is worn with its +x direction facing the other way,
