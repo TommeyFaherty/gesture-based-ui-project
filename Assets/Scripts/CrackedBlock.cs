2038c6e3293f0aa67bfd2753e8a3224a61c0f62b
TommeyFaherty <tommeyf3@hotmail.com>
2020-03-24 14:49:33 +0000

    Adding CrackedBlock script

---
+++ Assets/Scripts/CrackedBlock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrackedBlock : MonoBehaviour
+{
+    private float thrust = 200.0f;
+    private void OnCollisionEnter(Collision collision)
+    {
+       
+        Debug.Log("Collision detected");
+        StartCoroutine("CubeFalling");
+        
+    }
+    IEnumerator CubeFalling(){ 
+        yield return new WaitForSeconds(3.0f); 
+        Debug.Log("wait", this); 
+        transform.GetComponent<Rigidbody>().useGravity = true;
+        transform.GetComponent<Rigidbody>().isKinematic = false;
+        transform.GetComponent<Rigidbody>().AddForce(transform.up * thrust);
+        Debug.Log("done", this); 
+    } 
+}
