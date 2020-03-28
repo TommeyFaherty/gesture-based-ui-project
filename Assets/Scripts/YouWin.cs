29fe26842d7684d4753f2e7dbc0a1c8b071ea05b
Ronan-H <g00342393@gmit.ie>
2020-03-28 11:51:09 +0000

    Added You Win scene and trigger in Level 3

---
+++ Assets/Scripts/YouWin.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class YouWin : MonoBehaviour
+{
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadSceneAsync("MainMenu", LoadSceneMode.Single);
+    }
+}
