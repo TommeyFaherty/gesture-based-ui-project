914dd3f0e1d9fe107c0191cd5f65614a48cf07b3
Ronan Hanley <g00342393@gmit.ie>
2020-03-29 11:12:09 +0100

    Jumping relative to level rotation (#15)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,33 +3,40 @@ using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float jumpForce = 10f;
+    private float jumpForce = 7f;
     private float dashForce = 5f;
     private float cooldown = 0f;
     private float distToGround;
     Rigidbody rb;
 
     private MyoPose myoPose;
+    private LevelController levelController;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         distToGround = GetComponent<Collider>().bounds.extents.y;
-
         myoPose = FindObjectOfType<MyoPose>();
+        levelController = FindObjectOfType<LevelController>();
+    }
+
+    // normal angle of the level
+    private Vector3 GetLevelNormal()
+    {
+        return levelController.transform.up;
     }
 
     bool IsGrounded() {
-        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+        // raycast towards level
+        Vector3 inverseNormal = GetLevelNormal() * -1;
+        return Physics.Raycast(transform.position, inverseNormal, distToGround + 0.1f);
     }
 
     void Update()
     {
-        transform.localScale = transform.localScale;
-
         //Jump with ball if its on the ground
         if ((Input.GetKeyDown(KeyCode.Space) || myoPose.ConsumeFistIfDetected()) && IsGrounded()) {
-            Vector3 jumpVelocity = new Vector3(0f,jumpForce,0f);
+            Vector3 jumpVelocity = GetLevelNormal() * jumpForce;
             
             //Ensures ball maintains speed
             rb.velocity = rb.velocity + jumpVelocity; 
@@ -53,7 +60,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         // add some extra downward force to the player so that it doesn't bounce around the level as much
         rb.AddForce(1, -50, 1);
